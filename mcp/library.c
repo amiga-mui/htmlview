b030abaeb7f3f2ff16409cbed156ba5da131588b
Thore Böckelmann <tboeckel@gmx.de>
2007-07-25 19:14:12 +0000

    * #?/library.c: now really uses the 3 parameter version of GETINTERFACE()   defined in mccinit.c instead of the old obsolete version in mcc_common.h.   The user definable functions (i.e. ClassInit()) must be declared before   the inclusion of mccinit.c.

--- mcp/library.c
+++ mcp/library.c
@@ -63,6 +63,20 @@ struct LocaleBase *LocaleBase = NULL;
 struct LocaleIFace *ILocale = NULL;
 #endif
 
+/******************************************************************************/
+/* define the functions used by the startup code ahead of including mccinit.c */
+/******************************************************************************/
+
+static BOOL ClassInit(UNUSED struct Library *base);
+static VOID ClassExpunge(UNUSED struct Library *base);
+
+/******************************************************************************/
+/* include the lib startup code for the mcc/mcp  (and muimaster inlines)      */
+/******************************************************************************/
+
+#include "icon.bh"
+#include "mccinit.c"
+
 static BOOL ClassInit(UNUSED struct Library *base)
 {
   if((LocaleBase = (APTR)OpenLibrary("locale.library", 38)) &&
@@ -98,12 +112,3 @@ static VOID ClassExpunge(UNUSED struct Library *base)
   }
 }
 
-
-/******************************************************************************/
-/*                                                                            */
-/* include the lib startup code for the mcc/mcp  (and muimaster inlines)      */
-/*                                                                            */
-/******************************************************************************/
-
-#include "icon.bh"
-#include "mccinit.c"
